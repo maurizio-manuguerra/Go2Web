a1f8e6d7436217bbd112360dda84c03e7cf20c90
maurizio-manuguerra <maurizio.manuguerra@gmail.com>
2015-05-14 12:38:22 +1000

    - Name change from Go2Web to webserver.start - Function webserver.stop added to the package

--- R/functions.R
+++ R/functions.R
@@ -1,14 +1,24 @@
-#' @title Go2Web
+#' @title webserver.start
 #' @description This function publish the content of the current directory on a web server. Technically, 
 #' it runs the system command "python -m SimpleHTTPServer 8000 &>/dev/null". The default port is 8000 and 
 #' every output is redirected to /dev/null (i.e. not shown to the user). Both options can be changed by the user.
 #' @param port Defaults to 8000
 #' @param redirect.to.null Defaults to TRUE
-#' @examples Go2Web()
+#' @examples webserver.start()
 
-Go2Web <- function(port="8000", redirect.to.null=TRUE){
+webserver.start <- function(port="8000", redirect.to.null=TRUE){
   redirect=""
   if (redirect.to.null) redirect=" &>/dev/null"
   cmd <- paste("python -m SimpleHTTPServer ", port, redirect, " &", sep="")
+  system(cmd)
+}
+
+#' @title webserver.stop
+#' @description This function stops the web server started with ``webserver.start()''. The default port is 8000 and can be changed by the user to whatever port the server is running on.
+#' @param port Defaults to 8000
+#' @examples webserver.stop()
+
+webserver.stop <- function(port="8000"){
+  cmd <- paste("killall -e python -m SimpleHTTPServer ", port, sep="")
   system(cmd)
 }
