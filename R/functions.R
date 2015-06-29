c6bed2b50564030b965530410777cd902be98fd1
Maurizio Manuguerra <maurizio.manuguerra@gmail.com>
2015-06-29 20:17:40 +1000

    Added support for ruby (default)

--- R/functions.R
+++ R/functions.R
@@ -1,15 +1,19 @@
 #' @title webserver.start
 #' @description This function publish the content of the current directory on a web server. Technically, 
-#' it runs the system command "python -m SimpleHTTPServer 8000 &>/dev/null". The default port is 8000 and 
+#' it runs the system command "python -m SimpleHTTPServer 8004 &>/dev/null" or its ruby counterpart. The default port is 8004 and 
 #' every output is redirected to /dev/null (i.e. not shown to the user). Both options can be changed by the user.
-#' @param port Defaults to 8000
+#' @param port Defaults to 8004
 #' @param redirect.to.null Defaults to TRUE
 #' @examples webserver.start()
 
-webserver.start <- function(port=8000, redirect.to.null=TRUE){
+webserver.start <- function(port=8004, redirect.to.null=TRUE, lang=c('ruby', 'python')){
   redirect=""
   if (redirect.to.null) redirect=" &>/dev/null"
-  cmd <- paste("python -m SimpleHTTPServer ", as.character(port), redirect, " &", sep="")
+  if (lang=='python'){
+    cmd <- paste("python -m SimpleHTTPServer ", as.character(port), redirect, " &", sep="")
+  } else if (lang=='ruby'){
+    cmd <- paste("ruby -run -ehttpd . -p ", as.character(port), redirect, " &", sep="")
+  }
   system(cmd)
   .Last <<- eval(parse(text=paste("function() webserver.stop(port=",as.character(port),")")))
 }
@@ -17,12 +21,17 @@ webserver.start <- function(port=8000, redirect.to.null=TRUE){
 
 
 #' @title webserver.stop
-#' @description This function stops the web server started with ``webserver.start()''. The default port is 8000 and can be changed by the user to whatever port the server is running on.
-#' @param port Defaults to 8000
+#' @description This function stops the web server started with ``webserver.start()''. The default port is 8004 and can be changed by the user to whatever port the server is running on.
+#' @param port Defaults to 8004
 #' @examples webserver.stop()
 
-webserver.stop <- function(port=8000){
-  cmd <- paste("killall -e python -m SimpleHTTPServer ", as.character(port), sep="")
+webserver.stop <- function(port=8004, lang=c('ruby', 'python')){
+  if (lang=='python'){
+    cmd <- paste("killall -e python -m SimpleHTTPServer ", as.character(port), sep="")
+  } else if (lang=='ruby'){
+    cmd <- "killall -e ruby"
+  }
+
   system(cmd)
   rm(.Last, envir=.GlobalEnv)
 }
@@ -31,12 +40,15 @@ webserver.stop <- function(port=8000){
 #' @description This function checks if a python SimpleHTTPServer is running.
 #' @examples webserver.status()
 
-webserver.status <- function(){
+webserver.status <- function(lang=c('ruby', 'python')){
   x <- system(paste("ps aux"),intern = T)
-  running <- any(grepl("python -m SimpleHTTPServer",x))
+  if (lang=='python'){
+    running <- any(grepl("python -m SimpleHTTPServer",x))
+  } else if (lang=='ruby'){
+    running <- any(grepl("ruby", x))
   if (running) {
-    cat("'python -m SimpleHTTPServer' is running")
+    cat("The webserver is running")
   } else {
-    cat("'python -m SimpleHTTPServer' is NOT running")
+    cat("The webserver is NOT running")
   }
 }
